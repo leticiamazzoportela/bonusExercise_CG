b4a85cf7a14ad8bd074a352f4cc979ca285a2474
leticiamazzoportela <leticiaivpportela@hotmail.com>
2018-03-28 20:52:11 -0300

    Add translate operation

--- BonusExercise_1/bonusExercise_1.c
+++ BonusExercise_1/bonusExercise_1.c
@@ -1,64 +1,96 @@
 /**
 Descrição:
-Autora:
+Autora: Letícia Mazzo Portela.
 Data de Criação: 27/03/2018.
-Datas de Modificação: 28/03/2018.
 */
 
 #include <stdio.h>
 #include <GL/glut.h>
 
-/*p animação, eu posso colocar na função display:
-    var globais: double tx = 0, ty = 0;
-    display(){
-        ...
-        gltranslatef(tx, ty,0);
-    }
+//    calcular na mão o centroide: no inicio ele é 5,5
 
-    teclado(int key, int x, int y){ //tecla pressionada
-        if(tecla=="GLUT_KEY_RIGHT")
-            tx = tx+1; //eixo x
-        glutPostRedisplay();
-    }
+/*Cabeçalho das funções*/
+void display();
+int init();
+void keyboardControl(unsigned char key, int x, int y);
+void arrowKeysControl(int key, int x, int y);
+void draw();
 
-    calcular na mão o centroide: no inicio ele é 5,5
-*/
+/*Variaveis para controle dos eixos x e y*/
 double tx = 0, ty = 0;
 
-int init(void){
-    glClearColor(1.0, 1.0, 1.0, 0.0);     //define a cor de fundo
+int main(int argc, char** argv) {
 
-    glMatrixMode(GL_PROJECTION);          //carrega a matriz de projeção
-    gluOrtho2D(0.0,100.0,0.0,100.0);      //define projeção ortogonal 2D
-    //mudar essa linha de cima para ficar proporcional a 500x500
+    glutInit(&argc,argv);                                     
+    glutInitDisplayMode(GLUT_SINGLE| GLUT_RGB);               
+    glutInitWindowSize(500,500);                              
+    glutInitWindowPosition(200,0);
+    glutCreateWindow("Exercicio Bonus 1");          
 
+    init();                          //executa função de inicialização
+    glutDisplayFunc(display);        //estabelece a função "display" como a função callback de exibição.
+    glutKeyboardFunc(keyboardControl); 
+    glutSpecialFunc(arrowKeysControl);
+    glutMainLoop();                  
+    return 0;
 }
 
-void display(void){
-    glClear(GL_COLOR_BUFFER_BIT);         //desenha o fundo (limpa a janela)
+int init(){
+    glClearColor(1.0, 1.0, 1.0, 0.0);     
+    glMatrixMode(GL_PROJECTION);          
+    gluOrtho2D(0.0,100.0,0.0,100.0);      
+}
 
-    glColor3f(1.0,0.0,0.0);               //altera o atributo de cor
-    glBegin(GL_POLYGON);                    //desenha uma linha
+void draw(){
+    glBegin(GL_POLYGON);                  
         glVertex2f(0,0);
         glVertex2f(10,0);
         glVertex2f(10,10);
         glVertex2f(0,10);
     glEnd();
-
-    glFlush();                            //desenha os comandos não executados
 }
 
-int main(int argc, char** argv) {
-
-    glutInit(&argc,argv);                                     //inicializa o GLUT
-    glutInitDisplayMode(GLUT_SINGLE| GLUT_RGB);               //configura o modo de display
-    glutInitWindowSize(500,500);                              //configura a largura e altura da janela de exibição
-    glutInitWindowPosition(200,0);
-    glutCreateWindow("Exercicio Bonus 1");           //cria a janela de exibição
-
-    init();                          //executa função de inicialização
-    glutDisplayFunc(display);        //estabelece a função "display" como a função callback de exibição.
-    glutMainLoop();                  //mostre tudo e espere
-    return 0;
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT);         
+    glColor3f(1.0,0.0,0.0);               
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(tx, ty, 0);
+    //glRotatef();
+    //Falta fazer a rotação e pronto :)
+    
+    draw();
+    glFlush();                            
 }
 
+/*Função que controla as teclas normais do teclado*/
+void keyboardControl(unsigned char key, int x, int y){
+    switch(key){
+        case 'R':
+        case 'r': //add rotacao
+                  break;
+        case 'T':
+        case 't': //add translacao
+                  break;
+    }
+
+    glutPostRedisplay();
+}
+
+/*Função que controla as teclas especiais do teclado (Setas, no caso)*/
+void arrowKeysControl(int key, int x, int y){
+    if(key == GLUT_KEY_RIGHT){
+        tx = tx + 1;
+    }
+    if(key == GLUT_KEY_LEFT){
+        tx = tx - 1;
+    }
+    if(key == GLUT_KEY_UP){
+        ty = ty + 1;
+    }
+    if(key == GLUT_KEY_DOWN){
+        ty = ty + 1;
+    }
+    
+    glutPostRedisplay();
+}
