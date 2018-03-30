6a63d8ce43e8665b020f495138b58beeef312dc6
leticiamazzoportela <leticiaivpportela@hotmail.com>
2018-03-30 16:47:25 -0300

    Add final version

---
+++ BonusExercise_1/README.txt
@@ -0,0 +1,17 @@
+                                        Instruções
+
+Compilação:
+    gcc nomeAlgoritmo.c -lGL -lglut -lGLU
+
+Execução:
+    ./a.out
+
+Exemplo:
+    gcc bonusExercise_1.c -lGL -lglut -lGLU
+    ./a.out
+
+O programa inicia no modo "translação". Para
+passar para o modo "rotação", tecle r, para
+voltar para o modo "translação", tecle t.
+
+Obs: Instruções para sistema Linux com OpenGL instalado.
--- BonusExercise_1/bonusExercise_1.c
+++ BonusExercise_1/bonusExercise_1.c
@@ -1,5 +1,6 @@
 /**
-Descrição:
+Descrição: Programa em C que usa a API OpenGL. O programa possibilita que sejam feitas duas transformações
+           geométricas (translação e rotação) em um quadrado vermelho de lado 10.
 Autora: Letícia Mazzo Portela.
 Data de Criação: 27/03/2018.
 */
@@ -7,7 +8,6 @@ Data de Criação: 27/03/2018.
 #include <stdio.h>
 #include <GL/glut.h>
 
-//    calcular na mão o centroide: no inicio ele é 5,5
 
 /*Cabeçalho das funções*/
 void display();
@@ -16,8 +16,8 @@ void keyboardControl(unsigned char key, int x, int y);
 void arrowKeysControl(int key, int x, int y);
 void draw();
 
-/*Variaveis para controle dos eixos x e y*/
-double tx = 0, ty = 0;
+float tx = 0, ty = 0, theta; //Variaveis para controle dos eixos x e y e para o controle do angulo de rotacao
+unsigned char key_op = 't'; //controle das opções: translação (t) e rotação (r). Inicia na translação.
 
 int main(int argc, char** argv) {
 
@@ -27,20 +27,23 @@ int main(int argc, char** argv) {
     glutInitWindowPosition(200,0);
     glutCreateWindow("Exercicio Bonus 1");          
 
-    init();                          //executa função de inicialização
-    glutDisplayFunc(display);        //estabelece a função "display" como a função callback de exibição.
-    glutKeyboardFunc(keyboardControl); 
-    glutSpecialFunc(arrowKeysControl);
-    glutMainLoop();                  
+    init();                            //executa função de inicialização
+    glutDisplayFunc(display);          //estabelece a função "display" como a função callback de exibição.
+    glutKeyboardFunc(keyboardControl); //estabelece a função "keyboardControl" como a função callback de teclado
+    glutSpecialFunc(arrowKeysControl); //estabelece a função "arrowKeysControl" como a função callback para teclas especiais
+    glutMainLoop();  
+                    
     return 0;
 }
 
+/*Função de inicialização*/
 int init(){
     glClearColor(1.0, 1.0, 1.0, 0.0);     
     glMatrixMode(GL_PROJECTION);          
     gluOrtho2D(0.0,100.0,0.0,100.0);      
 }
 
+/*Função responsável por desenhar um quadrado de lado 10*/
 void draw(){
     glBegin(GL_POLYGON);                  
         glVertex2f(0,0);
@@ -50,15 +53,16 @@ void draw(){
     glEnd();
 }
 
+/*Função para exibição*/
 void display(){
     glClear(GL_COLOR_BUFFER_BIT);         
     glColor3f(1.0,0.0,0.0);               
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    glTranslatef(tx, ty, 0);
-    //glRotatef();
-    //Falta fazer a rotação e pronto :)
-    
+    glTranslatef(tx + 5, ty + 5, 0); //centroide do quadrado = 5,5 
+    glRotatef(theta, 0, 0, 1); //rotaciona o quadrado em um angulo theta usando centroide 5,5 como ponto de rotação
+    glTranslatef(-tx - 5, -ty - 5, 0); 
+    glTranslatef(tx, ty, 0); //faz a translação do quadrado
     draw();
     glFlush();                            
 }
@@ -67,10 +71,10 @@ void display(){
 void keyboardControl(unsigned char key, int x, int y){
     switch(key){
         case 'R':
-        case 'r': //add rotacao
+        case 'r': key_op = 'r';
                   break;
         case 'T':
-        case 't': //add translacao
+        case 't': key_op = 't';
                   break;
     }
 
@@ -78,18 +82,25 @@ void keyboardControl(unsigned char key, int x, int y){
 }
 
 /*Função que controla as teclas especiais do teclado (Setas, no caso)*/
-void arrowKeysControl(int key, int x, int y){
-    if(key == GLUT_KEY_RIGHT){
-        tx = tx + 1;
+void arrowKeysControl(int op, int x, int y){
+    if(key_op == 't'){
+        if(op == GLUT_KEY_RIGHT) //deslocada no eixo x à direita
+            tx = tx + 1;
+
+        if(op == GLUT_KEY_LEFT) //desloca no eixo x à esquerda
+            tx = tx - 1;
+
+        if(op == GLUT_KEY_UP) //desloca no eixo y para cima
+            ty = ty + 1;
+
+        if(op == GLUT_KEY_DOWN) //desloca no eixo y para baixo
+            ty = ty - 1;
     }
-    if(key == GLUT_KEY_LEFT){
-        tx = tx - 1;
-    }
-    if(key == GLUT_KEY_UP){
-        ty = ty + 1;
-    }
-    if(key == GLUT_KEY_DOWN){
-        ty = ty + 1;
+    if(key_op == 't'){
+        if(op == GLUT_KEY_RIGHT)
+            theta = theta - 1; //theta > 0: rotação anti-horaria
+        if(op == GLUT_KEY_LEFT)
+            theta = theta + 1; //theta < 0: rotação horária
     }
     
     glutPostRedisplay();
